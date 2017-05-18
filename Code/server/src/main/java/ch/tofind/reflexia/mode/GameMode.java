8a85176d874a0baab8910021d7fedd8d0f47ee35
abass-mahdavi <abass.mahdavi@heig-vd.ch>
2017-05-18 10:13:28 +0200

    added javadoc comments to Code/server/src/main/java/ch/tofind/reflexia/mode classes - abass

--- Code/server/src/main/java/ch/tofind/reflexia/mode/GameMode.java
+++ Code/server/src/main/java/ch/tofind/reflexia/mode/GameMode.java
@@ -4,11 +4,14 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
+/**
+ * @brief Game Mode class
+ */
 @XmlType(propOrder = { "name", "startingScore", "endingScore", "rounds" , "gameObjects"})
 @XmlRootElement(name = "mode")
 public class GameMode {
 
-    //! Game mode name
+    //! Game Mode name
     private String name;
 
     //! BestScore to start with
@@ -23,10 +26,19 @@ public class GameMode {
     //! Objects composed by the mode
     private GameObjects gameObjects;
 
+     //! @brief GameMode parameterless constructor
     protected GameMode() {
 
     }
 
+    /**
+     * @brief Game Mode constructor with parameters
+     * @param name name of the mode
+     * @param startingScore Starting score
+     * @param endingScore Ending score
+     * @param rounds Number of rounds
+     * @param gameObjects Objects composed by the game mode
+     */
     public GameMode(String name, Integer startingScore, Integer endingScore, Integer rounds, GameObjects gameObjects) {
         super();
         this.name = name;
@@ -36,49 +48,102 @@ public class GameMode {
         this.gameObjects = gameObjects;
     }
 
+    /**
+     * @brief gets the name of a game mode
+     * @return the name of the mode
+     */
     @XmlElement(name = "name")
     public String getName() {
         return name;
     }
+
+    /**
+     * @brief sets the name of a game mode
+     * @param name of a game mode
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @brief gets the starting score of a game mode
+     * @return the starting score
+     */
     @XmlElement(name = "startingScore")
     public Integer getStartingScore() {
         return startingScore;
     }
+
+    /**
+     * @brief sets the starting score of a game mode
+     */
     public void setStartingScore(Integer startingScore) {
         this.startingScore = startingScore;
     }
 
+    /**
+     * @brief gets the ending score of a game mode
+     * @return the ending score
+     */
     @XmlElement(name = "endingScore")
     public Integer getEndingScore() {
         return endingScore;
     }
+
+    /**
+     * @brief gets the ending score of a game mode
+     * @return the ending score
+     */
     public void setEndingScore(Integer endingScore) {
         this.endingScore = endingScore;
     }
 
+    /**
+     * @brief gets the number of rounds of a game mode
+     * @return the number of rounds
+     */
     @XmlElement(name = "rounds")
     public Integer getRounds() {
         return rounds;
     }
+
+    /**
+     * @brief sets the rounds of a game mode
+     * @param rounds of a game mode
+     */
     public void setRounds(Integer rounds) {
         this.rounds = rounds;
     }
 
+    /**
+     * @brief gets the gameObjects attributes of game mode
+     * @return the objects composed by the game mode
+     */
     @XmlElement(name = "gameObjects")
     public GameObjects getGameObjects() {
         return gameObjects;
     }
+
+    /**
+     * @brief sets the gameObjects attributes of game mode
+     * @param gameObjects
+     */
     public void setGameObjects(GameObjects gameObjects) {
         this.gameObjects = gameObjects;
     }
+
+    /**
+     * @brief adds a gameObject to the gameObjects of a game mode
+     * @param gameObject
+     */
     public void addGameObject(GameObject gameObject) {
         gameObjects.add(gameObject);
     }
 
+    /**
+     * @brief toString overloaded method for game mode
+     * @return a string representation of the game mode
+     */
     @Override
     public String toString() {
         return "GameMode"                            + '\n' + '\t' +
--- Code/server/src/main/java/ch/tofind/reflexia/mode/GameObject.java
+++ Code/server/src/main/java/ch/tofind/reflexia/mode/GameObject.java
@@ -4,23 +4,33 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * @brief game object class definition
+ */
 @XmlRootElement(name = "gameObject")
 public class GameObject {
 
-    //!
+    //! game Object type
     private String type;
 
-    //!
+    //! game Object current points
     private Integer points;
 
-    //!
+    //! game Object timeout
     private Integer timeout;
 
 
+    //! game Object constructor
     protected GameObject() {
 
     }
 
+    /**
+     * @brief game object constructor with parameters
+     * @param type
+     * @param points
+     * @param timeout
+     */
     public GameObject(String type, Integer points, Integer timeout) {
         super();
         this.type = type;
@@ -28,30 +38,61 @@ public class GameObject {
         this.timeout = timeout;
     }
 
+    /**
+     * @brief gets the type of a game object
+     * @return the type of a game object
+     */
     @XmlAttribute(name = "type")
     public String getType() {
         return type;
     }
+
+    /**
+     * @brief sets the type of a game object
+     * @param type of a game object
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * @brief gets the game object current points
+     * @return current points
+     */
     @XmlElement(name = "points")
     public Integer getPoints() {
         return points;
     }
+
+    /**
+     * @brief sets game object current points
+     * @param points
+     */
     public void setPoints(Integer points) {
         this.points = points;
     }
 
+    /**
+     * @brief gets game object time out
+     * @return game object time out
+     */
     @XmlElement(name = "timeout")
     public Integer getTimeout() {
         return timeout;
     }
+
+    /**
+     * @brief sets game object timeout
+     * @param timeout
+     */
     public void setTimeout(Integer timeout) {
         this.timeout = timeout;
     }
 
+    /**
+     * @brief toString function for a game object
+     * @return a string representation of a game object
+     */
     @Override
     public String toString() {
         return "Game object"          + '\n' + '\t' +
